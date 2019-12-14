8610e2788b4e223c524658b0dc035848f652eaf0
Zoe <zxenonwolf@gmail.com>
2019-12-14 14:07:16 +0100

    Cleaning unnecessary files & code

--- .bash_history
+++
@@ -1,3 +0,0 @@
-npm install mongodb@^3.0.0 --save
-npm i
-npm i
--- server.js
+++ server.js
@@ -1,35 +1,38 @@
 "use strict";
 
-var express = require("express");
-var dns = require("dns");
-var mongo = require("mongodb");
-var mongoose = require("mongoose");
-var bodyParser = require("body-parser");
-var cors = require("cors");
-var app = express();
-var port = process.env.PORT || 3000;
+const express = require("express");
+const dnsPromises = require("dns").promises;
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Mongo init
 mongoose.connect(process.env.MONGO_URI);
 
-var urlSchema = new mongoose.Schema({
+const urlSchema = new mongoose.Schema({
   original: String,
   new: Number
 });
-var Url = mongoose.model("Url", urlSchema);
+const Url = mongoose.model("Url", urlSchema);
 
+// Endpoints
 app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.get("/api/shorturl/:shortenedUrl", async function(req, res) {
+// URL SHORTENER - retrieve previously created URL
+app.get("/api/shorturl/:shortenedUrl", async function (req, res) {
   const findUrl = await Url.findOne({ new: req.params.shortenedUrl });
+
   if (findUrl) {
-    const originalUrl = findUrl.original;
+    const { original: originalUrl } = findUrl;
     const urlWithProtocol = originalUrl.includes("http")
       ? originalUrl
       : "https://" + originalUrl;
@@ -40,14 +43,13 @@ app.get("/api/shorturl/:shortenedUrl", async function(req, res) {
   }
 });
 
-app.post("/api/shorturl/new", async function(req, res) {
-  const oldUrl = req.body.url;
-  try {
-    const dnsPromises = dns.promises;
-    const REPLACE_REGEX = /^https?:\/\//i;
+// URL SHORTENER - create new shortened URL
+app.post("/api/shorturl/new", async function (req, res) {
+  const { url: oldUrl } = req.body;
 
-    const cleanOldUrl = oldUrl.replace(REPLACE_REGEX, "");
-    let data = await dnsPromises.lookup(cleanOldUrl);
+  try {
+    const urlWithoutProtocol = oldUrl.replace(/^https?:\/\//i, "");
+    await dnsPromises.lookup(urlWithoutProtocol);
   } catch (e) {
     res.json({
       error: "invalid URL"
@@ -57,22 +59,23 @@ app.post("/api/shorturl/new", async function(req, res) {
   const lastCreatedUrl = await Url.findOne()
     .sort({ field: "asc", _id: -1 })
     .exec();
-  console.log(lastCreatedUrl);
+
   try {
     const newUrl = new Url({
       original: oldUrl,
       new: lastCreatedUrl ? lastCreatedUrl.new + 1 : 1
     });
     const finalUrl = await newUrl.save();
+
     res.json({
       original_url: finalUrl.original,
       short_url: finalUrl.new
     });
   } catch (err) {
-    res.status(500).send("errrr" + err);
+    res.status(500).send(err);
   }
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("Node.js listening ...");
 });
